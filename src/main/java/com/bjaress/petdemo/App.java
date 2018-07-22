72784862f10defa265428ad039b9127c0d86098e
Brian Jaress <bjaress@gmail.com>
2018-07-21 17:43:24 -0700

    cleanup

---
+++ src/main/java/com/bjaress/petdemo/App.java
@@ -0,0 +1,13 @@
+package com.bjaress.petdemo;
+
+import java.util.Arrays;
+
+/**
+ * Demo app for searching through CSV
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println(Arrays.asList(args));
+    }
+}
--- src/test/java/com/bjaress/petdemo/AppTest.java
+++ src/test/java/com/bjaress/petdemo/AppTest.java
@@ -1,4 +1,4 @@
-package com.bjaress;
+package com.bjaress.petdemo;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
