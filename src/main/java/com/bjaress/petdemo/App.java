b725b7ad15e760c75c599345cebfcde19f5717ac
Brian Jaress <bjaress@gmail.com>
2018-07-21 19:23:51 -0700

    input and output in the right formats

--- src/main/java/com/bjaress/petdemo/App.java
+++ src/main/java/com/bjaress/petdemo/App.java
@@ -1,13 +1,56 @@
 package com.bjaress.petdemo;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.function.Function;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.BufferedWriter;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
 
 /**
  * Demo app for searching through CSV
  *
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println(Arrays.asList(args));
+
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        List<String> argList = Arrays.asList(args);
+
+        if (argList.isEmpty()) {
+            System.out.println("FAIL: You need to include an input filename.");
+            System.exit(-1);
+        }
+
+        Map<String, String> query = argList.stream().skip(1)
+            .map(arg -> Arrays.asList(arg.split("=")))
+            .filter(parts -> parts.size() > 1)
+            .collect(Collectors.toMap(
+                        parts -> parts.get(0),
+                        parts -> parts.get(1)));
+
+        CSVReader reader = new CSVReader(new FileReader(argList.get(0)));
+
+        CSVWriter writer = new CSVWriter(new OutputStreamWriter(System.out));
+
+        String[] header = reader.readNext();
+        writer.writeNext(header, true);
+
+        while(true) {
+            String[] line = reader.readNext();
+            if (line == null) { break; }
+
+            writer.writeNext(line, true);
+        }
+        writer.close();
+
     }
 }
